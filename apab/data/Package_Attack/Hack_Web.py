48ae9c8ad1f1696a7300bbe46e1689747700f7cc
BDEMAY0 <95568319+BDEMAY0@users.noreply.github.com>
2023-04-16 20:02:35 +0200

    BDM
    Add TLS

--- apab/data/Package_Attack/Hack_Web.py
+++ apab/data/Package_Attack/Hack_Web.py
@@ -1,5 +1,7 @@
 import requests
-
+import ssl
+import socket
+from pprint import pprint
 """
 Le fichier HackWeb.py contient la classe Hack_Web, qui est utilisée pour récupérer les informations sur les bannières des serveurs web. 
 Il envoie une requête HEAD aux ports 80 et 443 pour récupérer les en-têtes de réponse HTTP et les affiche ensuite à l'utilisateur.
@@ -24,3 +26,35 @@ class Entete:
             headers = response.headers
             return f"Bannière du serveur : " + headers.get("Server", "\n") + "Technologie de serveur web : " + headers.get("X-Powered-By", "\n") + "Type de contenu : " + headers.get("Content-Type", "\n")
 
+
+class TLSSecurityChecker:
+    def __init__(self, hostname, port):
+        self.hostname = hostname
+        self.port = port
+
+    def check(self):
+        try:
+            context = ssl.create_default_context()
+            conn = context.wrap_socket(socket.socket(socket.AF_INET), server_hostname=self.hostname)
+            conn.connect((self.hostname, self.port))
+            cert = conn.getpeercert()
+
+            print(f"Certificat pour {self.hostname}:")
+            pprint(cert)
+
+            protocol_version = conn.version()
+            cipher, version, bits = conn.cipher()
+
+            print(f"\nVersion du protocole SSL/TLS: {protocol_version}")
+            print(f"Chiffrement utilisé: {cipher}, {bits} bits")
+
+            if protocol_version in ["SSLv2", "SSLv3", "TLSv1", "TLSv1.1"]:
+                print("\nAttention: Le protocole SSL/TLS utilisé est obsolète et pourrait présenter des risques de sécurité.")
+
+        except Exception as e:
+            print(f"Erreur lors de la vérification de la sécurité SSL/TLS pour {self.hostname}: {e}")
+
+
+hostname = "ajdioqdivnoidn.com"  # Remplacez par le nom de domaine du site Web que vous souhaitez vérifier
+checker = TLSSecurityChecker(hostname)
+checker.check()
