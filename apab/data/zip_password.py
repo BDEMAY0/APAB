d6543cf630370b3f5bb038e2a180b115f530794f
BDEMAY0 <95568319+BDEMAY0@users.noreply.github.com>
2023-05-14 19:57:29 +0200

    Update zip_password.py - BDM

--- apab/data/zip_password.py
+++ apab/data/zip_password.py
@@ -4,25 +4,25 @@ import random
 import string
 import os
 
-
+def f_zip_encrypt(name_file):
 # Définir les caractères autorisés
-caracteres = string.ascii_letters + string.digits
+    caracteres = string.ascii_letters + string.digits
 
 # Générer une chaîne aléatoire de 20 caractères
-mot_de_passe = ''.join(random.choice(caracteres) for i in range(20))
+    mot_de_passe = ''.join(random.choice(caracteres) for i in range(20))
 
 
-current_dir = os.path.dirname(os.path.abspath(__file__))
+    current_dir = os.path.dirname(os.path.abspath(__file__))
 
-inpt = f'{current_dir}/rapport/rapport_audit_pentest.pdf'
-pre = None
-oupt = f'{current_dir}/ressources/mail/rapport_audit_pentest.zip'
-com_lvl = 5
+    inpt = f'{current_dir}/rapport/{name_file}'
+    pre = None
+    oupt = f'{current_dir}/ressources/mail/{name_file}.zip'
+    com_lvl = 5
 
 # compressing file
-pyminizip.compress(inpt, None, oupt,mot_de_passe, com_lvl)
+    pyminizip.compress(inpt, None, oupt,mot_de_passe, com_lvl)
 
-print(mot_de_passe)
+    return mot_de_passe    
 
 
 
