77d3e69c546544941650f4bdb0a97c849c7fd3a2
BDEMAY0 <95568319+BDEMAY0@users.noreply.github.com>
2023-05-19 19:18:26 +0200

    BDM
    +Rapport

--- apab/data/main.py
+++ apab/data/main.py
@@ -1,8 +1,10 @@
-from Package_Manage.Parser import NmapParser
+from Package_Manage.NmapParser import NmapParser
 import os
+from Package_Manage.launcher import * 
+from Package_Export.ManageExport import ManageExport
 
 # Le point d'entrée du programme
-if __name__ == "__main__":
+def main():
 
     # Chemin du répertoire courant
     current_dir = os.path.dirname(os.path.abspath(__file__))
@@ -14,11 +16,13 @@ if __name__ == "__main__":
     # Création d'un objet NmapParser et chargement du fichier JSON
     parser = NmapParser(nmap_output_file)
 
+    #Appel de fonction d'audit qui est considérer par défaut et comme cela n'est pas dans le fichier texte
+    #Cela va traiter le Netbios et les bannières des services et les ports tel que http
+    audit(parser)
     # Récupère les attaques cochées de l'application
     with open(parameter_file, "r+") as f:
         lignes = f.readlines()
         for i, ligne in enumerate(lignes):
-            print(ligne)
             key, value = ligne.strip().split(" :")
             if value == " True":
                 if isinstance(key, str) and key in globals():
@@ -26,7 +30,11 @@ if __name__ == "__main__":
             if key == "En cours":
                 lignes[i] = "En cours : finis       "
                 break
+               
         f.seek(0)
         f.writelines(lignes)
+        ManageExport.export_all_to_json("rapport_attaques.json")
 
 
+if __name__ == "__main__":
+    main()
