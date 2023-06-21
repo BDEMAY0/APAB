2893ec50a625fb9fd41f13dc5a7e3f77016b187d
BDEMAY0 <95568319+BDEMAY0@users.noreply.github.com>
2023-06-21 21:03:04 +0200

    BDM - EDIT JSON

--- data/Package_Export/CVE_Export.py
+++ data/Package_Export/CVE_Export.py
@@ -5,13 +5,21 @@ class CVE_Export(ManageExport):
         super().__init__(attack_name)
 
     def add_cve_host(self, host_ip, cve_info_list, product, version):
-        details = []
+       
+        new_details = []
         for cve_info in cve_info_list:
             cve_info["product"] = product
             cve_info["version"] = version
-            details.append(cve_info)
+            new_details.append(cve_info)
+
+        # Si l'hote est déjà présent dans l'objet
+        for host in self.hosts:
+            if host["ip_address"] == host_ip:
+                # Si l'host existe alors ajoute les détail à l'ip existante (evite de créer une autre ip similaire dans le json)
+                host["details"].extend(new_details)
+                return
 
         self.hosts.append({
             "ip_address": host_ip,
-            "details": details
+            "details": new_details
         })
