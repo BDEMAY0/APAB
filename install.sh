23b2a417d2fb9b1c034bfd04dcac91742461eb9a
BDEMAY0 <95568319+BDEMAY0@users.noreply.github.com>
2023-06-28 21:38:35 +0200

    BDM - Add SMBClient

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@
 sudo apt-get update
 
 # Installation des dépendances système
-sudo apt-get install -y python3-paramiko build-essential git python3-dev ffmpeg libsdl2-dev libsdl2-image-dev libsdl2-mixer-dev libsdl2-ttf-dev libportmidi-dev libswscale-dev libavformat-dev libavcodec-dev zlib1g-dev libgstreamer1.0-dev gstreamer1.0-plugins-{bad,base,good,ugly} gstreamer1.0-{omx,alsa} python3-setuptools libmtdev1 libpango1.0-dev lib32stdc++6 aircrack-ng
+sudo apt-get install -y python3-paramiko build-essential git python3-dev ffmpeg libsdl2-dev libsdl2-image-dev libsdl2-mixer-dev libsdl2-ttf-dev libportmidi-dev libswscale-dev libavformat-dev libavcodec-dev zlib1g-dev libgstreamer1.0-dev gstreamer1.0-plugins-{bad,base,good,ugly} gstreamer1.0-{omx,alsa} python3-setuptools libmtdev1 libpango1.0-dev lib32stdc++6 aircrack-ng smbclient
 
 # Installation des dépendances Python via pip
 pip3 install -r requirements.txt
