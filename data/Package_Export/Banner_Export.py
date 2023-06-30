542252478aa18f35d42789bc174eefb99284d4d6
BDEMAY0 <95568319+BDEMAY0@users.noreply.github.com>
2023-06-30 20:54:18 +0200

    BDM - EDIT MULTIPLE HOST

--- data/Package_Export/Banner_Export.py
+++ data/Package_Export/Banner_Export.py
@@ -5,13 +5,20 @@ class Banner_Export(ManageExport):
         super().__init__(attack_name)
 
     def add_banner_host(self, host_ip, port, service, version):
-        details = []
+        new_details = []
         banniere = {}
         banniere["Port"] = port
         banniere["Service"] = service
         banniere["Version"] = version
-        details.append(banniere)
+        new_details.append(banniere)
 
+         # Si l'hote est déjà présent dans l'objet
+        for host in self.hosts:
+            if host["ip_address"] == host_ip:
+                # Si l'host existe alors ajoute les détail à l'ip existante (evite de créer une autre ip similaire dans le json)
+                host["details"].extend(new_details)
+                return
+                
         self.hosts.append({
             "ip_address": host_ip,
             "details": details
